a11ef6be0d1b4edea8df342a4a85f42dfd85abbd
Isaac Malin <Isaacmalin3530@gmail.com>
2023-09-13 15:02:34 -0600

    Set up state for the application

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
 
-const URL = 'http://localhost:9000/api/todos'
+import TodoList from "./TodoList"
+import axios from "axios";
+
+const URL = "http://localhost:9000/api/todos";
 
 export default class App extends React.Component {
+  state = {
+    todos: [],
+    error: ''
+  };
+
+  fetchAllTodos = () => {
+    axios.get(URL)
+      .then(res => {
+        
+        this.setState({ ...this.state.todos, todos: res.data.data})
+        console.log(this.state.todos);
+      })
+      .catch(err => {
+        this.setState({ ...this.state, error: err.response.data.message })
+      })
+  }
+  componentDidMount() {
+    this.fetchAllTodos()
+  }
+
   render() {
-    return null
+    return (
+      <div>
+        <h2>Error: {this.state.error}</h2>
+        <h2>Todos:</h2>
+        {
+          this.state.todos.map(td => {
+            return <div key={td.id}>{td.name}</div>
+          })
+        }
+      </div>
+    )
   }
 }
--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -1,7 +1,18 @@
 import React from 'react'
 
 export default class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const { todos } = this.props
+    console.log(todos);
+  }
+
   render() {
-    return null
+    return (
+      <div>
+
+      </div>
+    )
   }
 }
